5f473cd66e6e6f01300393929a601389ddde8ce5
nikita <nikilodion87@gmail.com>
2025-04-25 21:49:25 +0300

    processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 def filter_by_state(dictionaries, state='EXECUTED'):
+    '''Функция фильтрует список словарей по значению ключа state'''
     result = []
     for item in dictionaries:
         if item.get('state') == state:
@@ -9,6 +10,7 @@ print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03
 
 
 def sort_by_date(glossary, reverse=True):
+    '''функция сортирует список словарей по значению ключа date'''
     return sorted(glossary, key=lambda x: x['date'], reverse=reverse)
 
 print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
